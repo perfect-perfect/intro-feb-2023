b6b9e9d939886522b27eaa6644a09988a807c4bc
unknown <gilberto_j_covarrubias_merino@progressive.com>
2023-02-06 15:26:41 -0500

    saving code before deleting and moving on

---
+++ day1/Finger/Program.cs
@@ -0,0 +1,14 @@
+﻿using Finger;
+System.Console.Write("What is your status: ");
+string? status = Console.ReadLine();
+// Type identifier = new Constructor
+if (status != null)
+{
+    StatusMessage myStatus = new StatusMessage(status, DateTimeOffset.Now);
+    Console.WriteLine($"You said your status was {myStatus.Status} at {myStatus.When:T}");
+}
+else
+{
+    Console.WriteLine("Sorry, cannot have a null status");
+}
+
---
+++ day1/Finger/StatusMessage.cs
@@ -0,0 +1,7 @@
+// PascalCasing for C#
+namespace Finger
+{
+    // record in my code when i need something that aggregates two pieces of data. this ties the string of "Status" with the time of "DateTimeOffset"
+    public record StatusMessage(string Status, DateTimeOffset When);
+
+}
