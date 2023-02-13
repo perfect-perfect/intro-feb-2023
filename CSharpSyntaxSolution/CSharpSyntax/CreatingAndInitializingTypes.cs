ce463f44dade897a2f1f1ca6fc00ff86d7bc2225
ITUStudent <gilberto_j_covarrubias_merino@progressive.com>
2023-02-13 10:27:42 -0500

    with jeff notes

--- CSharpSyntaxSolution/CSharpSyntax/CreatingAndInitializingTypes.cs
+++ CSharpSyntaxSolution/CSharpSyntax/CreatingAndInitializingTypes.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System.Text;
+using System.Xml.Linq;
 
 namespace CSharpSyntax;
 public class CreatingAndInitializingTypes
@@ -8,8 +10,9 @@ public class CreatingAndInitializingTypes
     [Fact]
     public void UsingLiteralsToCreateInstancesOfTypes()
     {
-        // local vaiabled -- variables that are declared inside a method, or a porperty
-        string myName = "Jeff"; // Initializing
+        // local variabled -- variables that are declared inside a method, or property.
+
+        string myName = "Jeff"; // Initializing.
         char mi = 'M';
 
         bool isVendor = true;
@@ -25,8 +28,9 @@ public class CreatingAndInitializingTypes
     }
 
     [Fact]
-    public void ImplicitlyTypedLocalVariables()
+    public void ImplictlyTypedLocalVariables()
     {
+        // Var can be used for local variables, and you must initialize the variable.
         var myAge = 21;
 
         var r = new TransitoryPolicyCommuterRecord();
@@ -36,9 +40,12 @@ public class CreatingAndInitializingTypes
 
         var myPay = 25.23M;
 
-        // option in c# 6, we don't use this much except in special circumstances I'll show later
+        // option in c# 6, we don't use this much except in special circumstances I'll show later.
+
         Taco lunch = new();
         Assert.IsType<Taco>(lunch);
+
+
     }
 
     [Fact]
@@ -56,73 +63,59 @@ public class CreatingAndInitializingTypes
     }
 
     [Fact]
-    public void MutableStringWithStringBuilders()
+    public void MutableStringsWithStringBuilders()
     {
         var message = new StringBuilder();
 
-        foreach(var num in Enumerable.Range(1,100000))
+        foreach (var num in Enumerable.Range(1, 10000))
         {
             message.Append(num.ToString() + ", ");
         }
         Assert.True(message.ToString().StartsWith("1, 2, 3, 4"));
 
         var myName = "Joe";
+
     }
 
     [Fact]
     public void MoreAboutStrings()
     {
-        var name = "Bob"; 
-        var age = 33; 
+        var name = "Bob";
+
+        var age = 33;
 
         var message = "The name is " + name + " and the age is " + age + ".";
-
         var message2 = string.Format("The name is {0} and the age is {1} (again, name: {0})", name, age);
-
         var pay = 120_000.00M;
-
         var m3 = $"{name} makes {pay:c} a year";
+
+
     }
 
     [Fact]
     public void DoingConversionsOnTypes()
     {
-        string myPay = "1000.83Tacos";
 
-        try
-        {
-            decimal payAsNumber = decimal.Parse(myPay);
+        string myPay = "10000.83";
 
-            Assert.Equal(10_000.83M, payAsNumber);
-
-        }
-        catch(FormatException) 
-        { 
-            // That didn't workd
-        }
 
         if(decimal.TryParse(myPay, out decimal payAsNumber))
         {
             Assert.Equal(10_000.83M, payAsNumber);
-        }else
+        } else
         {
-            Assert.True(false);
+            Assert.True(false); // it should blow up here.
+            
         }
 
         var birthdate = DateTime.Parse("04/20/1969");
         Assert.Equal(4, birthdate.Month);
         Assert.Equal(20, birthdate.Day);
         Assert.Equal(1969, birthdate.Year);
-
     }
-}
 
-public class Taco
-{
 
 }
 
-public class TransitoryPolicyCommuterRecord
-{
-
-}
+public class Taco { }
+public class TransitoryPolicyCommuterRecord { }
---
+++ CSharpSyntaxSolution/CSharpSyntax/Properties.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Principal;
+namespace CSharpSyntax;
+public class Properties
+{
+    [Fact]
+    public void WorkingWithTheAccount()
+    {
+        var account = new Account(); account.Name = "Bob"; // setting the name to "Bob":         Assert.Equal("Bob", account.Name); // Get the name from the object         account.Name = "Robert";         Assert.Equal(5000M, account.Balance);         //account.Balance = 100000;
+        account.Deposit(1000);
+        Assert.Equal(6000M, account.Balance); 
+        account.AccountNumber = 1000;
+        Assert.Equal(1000, account.AccountNumber);
+        // Properties:
+        // Should not throw exceptions. 
+        // Properties should return the same value every time you call them
+        //  unless the user changed that value.
+    }
+    [Fact]
+    public void ObjectInitializers()
+    {
+        var account = new Account()
+        {
+            AccountNumber = 1000,
+            Name = "Sue"
+        };
+        Assert.Equal("Sue", account.Name);
+        Assert.Equal(1000, account.AccountNumber);
+    }
+    public class Account
+    {
+        //public Account(int accountNumber)
+        //{         //    AccountNumber = accountNumber;
+        //}
+        //public Account(string name)
+        //{
+        //    Name = name;
+        //}         //public Account(int accountNumber, string name)
+        //{
+        //    AccountNumber = accountNumber;
+        //    Name = name;
+        //}
+        // private data - "backing field"
+        private string _name = "";
+        private decimal _balance = 5000M;
+        public required string Name // Property - they can get, set (or any other combination)
+        {
+            get { return _name; }
+            init { _name = value; }
+        }
+        // This isn't really a language thing. It is a compiler directive that says:
+        // Create a backing field for the account number.
+        // Create the "get and set" methods to mutate and access that backing field.
+        public required int AccountNumber { get; init; }         // This is a bit smelly according to Jeff
+        public decimal Balance
+        {
+            get { return _balance; }
+        }
+        public void Deposit(decimal amount)
+        {
+            // AccountNumber += 1;
+            _balance += amount;
+        }
+    }
+}
