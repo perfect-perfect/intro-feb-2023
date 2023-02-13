77bf17c99ba1b0f3aee7004906cb82ae92801856
ITUStudent <gilberto_j_covarrubias_merino@progressive.com>
2023-02-13 09:42:12 -0500

    some notes

---
+++ Backend/LearningResourcesSolution/LearningResourcesApi.IntegrationTests/StatusResourceTests.cs
@@ -0,0 +1,27 @@
+﻿using LearningResourcesApi.Controllers;
+
+namespace LearningResourcesApi.IntegrationTests;
+
+public class StatusResourceTests
+{
+    [Fact]
+    public async Task TheStatusResource()
+    {
+        // alba is a tool for better integration teesting against ASP.Net Core Applications
+        await using  var host = await AlbaHost.For<Program>();
+
+        var response = await host.Scenario(api => // Integration test - usually has many steps.
+        {
+            api.Get.Url("/status");
+            api.StatusCodeShouldBeOk(); // 200 status code.
+
+        });
+
+        var responseMessage = response.ReadAsJson<GetStatusResponse>();
+
+        Assert.NotNull(responseMessage);
+        Assert.Equal("All Good", responseMessage.Message);
+        Assert.Equal("555 555-5555", responseMessage.Contact);
+
+    }
+}
---
+++ Backend/LearningResourcesSolution/LearningResourcesApi/Controllers/StatusController.cs
@@ -0,0 +1,38 @@
+﻿using LearningResourcesApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningResourcesApi.Controllers;
+
+// this class inherits ControllerBase which extends some useful http request methods
+public class StatusController : ControllerBase
+{
+    private ISystemTime _systemTime;
+
+    public StatusController(ISystemTime systemTime)
+    {
+        _systemTime = systemTime;
+    }
+
+
+    // GET /status
+    [HttpGet("/status")]
+    public ActionResult GetTheStatus()
+    {
+        // look this up from a database or whatever
+        // If it is between midnight and 4 PM use a phone number,
+        // outside of that, use an email address.
+        var contact = _systemTime.GetCurrent().Hour < 16 ? "bob@aol.com" : "555 555-5555";
+        var response = new GetStatusResponse
+        {
+            Message = "All Good",
+            Contact = contact
+        };
+        return Ok(response);
+    }
+}
+
+public class GetStatusResponse
+{
+    public string Message { get; set; } = string.Empty;
+    public string Contact { get; set; } = string.Empty;
+}
